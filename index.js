5d4e168f3684d2dd32f2f8b447d0e2356adfdf53
berwin <berwin1995@qq.com>
2019-01-10 17:20:39 +0800

    First version

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-export default function ts (gen) {}
+export default function ts (gen) {
+  if (typeof gen === 'function') gen = gen()
+  if (!gen || typeof gen.next !== 'function') return
+
+  (function next () {
+    const res = gen.next()
+    if (res.done) return
+    setTimeout(next)
+  })()
+}
